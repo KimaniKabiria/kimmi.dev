44c921d80c5e5c16ae6edbfac7e4a71a82127f30
Kimani Kabiria <kabiriakimani@gmail.com>
2022-05-26 12:27:00 +0300

    Added rough notation

--- components/landing/WorkProcess.js
+++ components/landing/WorkProcess.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from "react"
 import { motion, useAnimation } from "framer-motion"
 import { useInView } from "react-intersection-observer"
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 import { MdDraw} from 'react-icons/md'
 import { GiNotebook } from "react-icons/gi";
@@ -25,7 +26,14 @@ export default function WorkProcess(){
                         <div className="flex w-16 justify-end items-end bg-gray-900 text-white dark:bg-white dark:text-black hover:bg-kimmi-teal dark:hover:bg-kimmi-teal rounded-lg -ml-8">  
                             <HiLightBulb className="w-10 h-10 py-2 pr-4 text-right" size='fa-2x' />
                         </div>
-                        <h5 className="text-base font-bold underline py-2 tracking-tight text-gray-900 dark:text-gray-300">Discover</h5>
+                        <h5 className="text-base font-bold py-2 tracking-tight text-gray-900 dark:text-gray-300">
+                            <RoughNotation 
+                                type="underline"
+                                // show = {true}
+                                padding={2}
+                                animate={false}>
+                            Discover</RoughNotation>
+                            </h5>
                         <p className="mb-3 text-sm font-normal text-gray-700 dark:text-white">I conduct user research to identify the problem I want to solve.</p>
                     </div>
                 </motion.div>
@@ -37,7 +45,12 @@ export default function WorkProcess(){
                         <div className="flex w-16 justify-end items-end bg-gray-900 text-white dark:bg-white dark:text-black hover:bg-kimmi-teal dark:hover:bg-kimmi-teal rounded-lg -ml-8">  
                             <GiNotebook className="w-10 h-10 py-2 pr-4 text-right" size='fa-2x' />
                         </div>
-                        <h5 className="text-base font-bold underline py-2 tracking-tight text-gray-900 dark:text-gray-300">Define</h5>
+                        <h5 className="text-base font-bold py-2 tracking-tight text-gray-900 dark:text-gray-300">
+                            <RoughNotation 
+                                type="underline"
+                                // show = {true}
+                                padding={2}
+                                animate={false}>Define</RoughNotation></h5>
                         <p className="mb-3 text-sm font-normal text-gray-700 dark:text-white">I brainstorm possible solutions to the identified problem.</p>
                     </div>
                 </motion.div>
@@ -49,7 +62,13 @@ export default function WorkProcess(){
                         <div className="flex w-16 justify-end items-end bg-gray-900 text-white dark:bg-white dark:text-black hover:bg-kimmi-teal dark:hover:bg-kimmi-teal rounded-lg -ml-8">  
                             <MdDraw className="w-10 h-10 py-2 pr-4 text-right" size='fa-2x' />
                         </div>
-                        <h5 className="text-base font-bold underline py-2 tracking-tight text-gray-900 dark:text-gray-300">Ideate</h5>
+                        <h5 className="text-base font-bold py-2 tracking-tight text-gray-900 dark:text-gray-300">
+                            <RoughNotation 
+                                type="underline"
+                                // show = {true}
+                                padding={2}
+                                animate={false}>Ideate</RoughNotation>
+                                </h5>
                         <p className="mb-3 text-sm font-normal text-gray-700 dark:text-white">I create wireframes and sketches of the product I’m about to create.</p>
                     </div>
                 </motion.div>
@@ -61,7 +80,12 @@ export default function WorkProcess(){
                         <div className="flex w-16 justify-end items-end bg-gray-900 text-white dark:bg-white dark:text-black hover:bg-kimmi-teal dark:hover:bg-kimmi-teal rounded-lg -ml-8">  
                             <FaShapes className="w-10 h-10 py-2 pr-4 text-right" size='fa-2x' />
                         </div>
-                        <h5 className="text-base font-bold underline py-2 tracking-tight text-gray-900 dark:text-gray-300">Prototype</h5>
+                        <h5 className="text-base font-bold py-2 tracking-tight text-gray-900 dark:text-gray-300">
+                            <RoughNotation 
+                                type="underline"
+                                // show = {true}
+                                padding={2}
+                                animate={false}>Prototype</RoughNotation></h5>
                         <p className="mb-3 text-sm font-normal text-gray-700 dark:text-white">I create high fidelity design and prototype the screens.</p>
                     </div>
                 </motion.div>
@@ -73,7 +97,12 @@ export default function WorkProcess(){
                         <div className="flex w-16 justify-end items-end bg-gray-900 text-white dark:bg-white dark:text-black hover:bg-kimmi-teal dark:hover:bg-kimmi-teal rounded-lg -ml-8">  
                             <AiTwotoneCode className="w-10 h-10 py-2 pr-4 text-right" size='fa-2x' />
                         </div>
-                        <h5 className="text-base font-bold underline py-2 tracking-tight text-gray-900 dark:text-gray-300">Implement</h5>
+                        <h5 className="text-base font-bold py-2 tracking-tight text-gray-900 dark:text-gray-300">
+                            <RoughNotation 
+                                type="underline"
+                                // show = {true}
+                                padding={2}
+                                animate={false}>Implement</RoughNotation></h5>
                         <p className="mb-3 text-sm font-normal text-gray-700 dark:text-white">Now I build, code and deliver for implementation.</p>
                     </div>
                 </motion.div>
